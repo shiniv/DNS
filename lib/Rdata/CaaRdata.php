cb692b05b26a0540ab3c757bf67f7cf336f985df
shiniv <shiniv.ning@gmail.com>
2017-10-04 13:51:51 +0800

    add caa record

---
+++ lib/Rdata/CaaRdata.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of Badcow DNS Library.
+ *
+ * (c) Samuel Williams <sam@badcow.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Badcow\DNS\Rdata;
+
+use Badcow\DNS\Validator;
+
+class CaaRdata implements RdataInterface
+{
+    use RdataTrait;
+
+    const TYPE = 'CAA';
+
+    /**
+     * @var string
+     */
+    protected $target;
+
+    /**
+     * @param $target
+     *
+     * @throws RdataException
+     */
+    public function setTarget($target)
+    {
+        if (!Validator::validateFqdn($target, false)) {
+            throw new RdataException(sprintf('The target "%s" is not a Fully Qualified Domain Name', $target));
+        }
+
+        $this->target = $target;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function output()
+    {
+        return '0 issue "'.$this->target.'"';
+    }
+}
